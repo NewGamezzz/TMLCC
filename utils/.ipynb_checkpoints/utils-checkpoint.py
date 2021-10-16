08ba39d47bf3dc825b9cbf6003e696e5e2732a43
NewGamezzz <folk48560@gmail.com>
2021-10-16 19:47:24 +0700

    add gridhist features

--- utils/.ipynb_checkpoints/utils-checkpoint.py
+++ utils/.ipynb_checkpoints/utils-checkpoint.py
@@ -1,5 +1,60 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from rdkit import Chem
+import math
+import pandas as pd
+import pickle
+import torch
+
+x_map = {
+    'atomic_num':
+    list(range(0, 119)),
+    'chirality': [
+        'CHI_UNSPECIFIED',
+        'CHI_TETRAHEDRAL_CW',
+        'CHI_TETRAHEDRAL_CCW',
+        'CHI_OTHER',
+    ],
+    'degree':
+    list(range(0, 11)),
+    'formal_charge':
+    list(range(-5, 7)),
+    'num_hs':
+    list(range(0, 9)),
+    'num_radical_electrons':
+    list(range(0, 5)),
+    'hybridization': [
+        'UNSPECIFIED',
+        'S',
+        'SP',
+        'SP2',
+        'SP3',
+        'SP3D',
+        'SP3D2',
+        'OTHER',
+    ],
+    'is_aromatic': [False, True],
+    'is_in_ring': [False, True],
+}
+
+e_map = {
+    'bond_type': [
+        'misc',
+        'SINGLE',
+        'DOUBLE',
+        'TRIPLE',
+        'AROMATIC',
+    ],
+    'stereo': [
+        'STEREONONE',
+        'STEREOZ',
+        'STEREOE',
+        'STEREOCIS',
+        'STEREOTRANS',
+        'STEREOANY',
+    ],
+    'is_conjugated': [False, True],
+}
 
 def check_nan_inf(X):
     return np.isnan(X).sum()+np.isinf(X).sum()
@@ -107,3 +162,45 @@ def target_encoder(df, X, y, features_idx, feature_encode):
 
     X[:, features_idx[feature_encode]] = encode.squeeze()
     return feature_mean
+
+def generate_graph(mol):
+    # Create Node Features
+    xs = []
+    for atom in mol.GetAtoms():
+        x = []
+        x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
+        x.append(x_map['chirality'].index(str(atom.GetChiralTag())))
+        x.append(x_map['degree'].index(atom.GetTotalDegree()))
+        x.append(x_map['formal_charge'].index(atom.GetFormalCharge()))
+        x.append(x_map['num_hs'].index(atom.GetTotalNumHs()))
+        x.append(x_map['num_radical_electrons'].index(atom.GetNumRadicalElectrons()))
+        x.append(x_map['hybridization'].index(str(atom.GetHybridization())))
+        x.append(x_map['is_aromatic'].index(atom.GetIsAromatic()))
+        x.append(x_map['is_in_ring'].index(atom.IsInRing()))
+        xs.append(x)
+    x = torch.tensor(xs, dtype=torch.float).view(-1, 9)
+    
+    # Create Edge Features
+    edge_indices, edge_attrs = [], []
+    for bond in mol.GetBonds():
+        i = bond.GetBeginAtomIdx()
+        j = bond.GetEndAtomIdx()
+
+        e = []
+        e.append(e_map['bond_type'].index(str(bond.GetBondType())))
+        e.append(e_map['stereo'].index(str(bond.GetStereo())))
+        e.append(e_map['is_conjugated'].index(bond.GetIsConjugated()))
+
+        edge_indices += [[i, j], [j, i]]
+        edge_attrs += [e, e]
+
+    edge_index = torch.tensor(edge_indices)
+    edge_index = edge_index.t().to(torch.long).view(2, -1)
+    edge_attr = torch.tensor(edge_attrs, dtype=torch.long).view(-1, 3)
+
+    # Sort indices.
+    if edge_index.numel() > 0:
+        perm = (edge_index[0] * x.size(0) + edge_index[1]).argsort()
+        edge_index, edge_attr = edge_index[:, perm], edge_attr[perm]
+        
+    return x, edge_index, edge_attr
